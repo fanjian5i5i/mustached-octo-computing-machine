b800e799ca857e591970510aaf629a95e358638f
fanjian5i5i <fanjian5i5i@gmail.com>
2014-04-21 23:38:20 -0400

    Changed for Presentation

--- js/functions.js
+++ js/functions.js
@@ -6,69 +6,36 @@ dojo.require("dijit.layout.ContentPane");
 dojo.require("dojox.layout.FloatingPane"); 
 
 dojo.require("dojo.parser");
-//dojo.require(["dojo/parser","dojo/on", "dojo/dom"]);
 
-	var pos = 0;
-	var pos1 = 400;
-	var horizontal = [];
-	var vertical = [];
-	var fillPosition = 5;
-	var num = 400;
-	
-	for(var i = 0; i < 400; ++i) {
-        horizontal[i] = fillPosition;
-		vertical[i] = fillPosition;
-        fillPosition += 5;
-		
-}
-
-
-	function fishSwim() {
-        document.getElementById("fish1").style.left = horizontal[pos] + "px";
-		document.getElementById("fish1").style.top = vertical[pos] + "px";
-        ++pos;
-        if (pos == num) {
-			
-            pos = 0;
-	}
-	var tim1 = setTimeout(fishSwim, 50);
-	}
-	
-	
-	function fishSwim1() {
-        document.getElementById("fish2").style.left = horizontal[pos1] + "px";
-		--pos1;
-        if (pos1 == 0) {
-			//setTimeout(pos = 200, 50000);
-            pos1 = 400;
-	}
-	var tim2 = setTimeout(fishSwim1, 20);
-	}
 	function drawChart(marker,inArray) {
 		var size = inArray.length;
 		
         // Create the data table.
         var data = new google.visualization.DataTable();
-        data.addColumn('number', 'year');
+        data.addColumn('string', 'year');
         data.addColumn('number', '');
 		var rows = new Array();
 		for(var i = 2;i<size;i++)
 		{
-			rows.push([i+1986,inArray[i]])
+			var year = (i+1986).toString();
+			rows.push([year,inArray[i]])
 		 };
 		 data.addRows(rows);
 
         // Set chart options
-        var options = {'title':'Annual February Precipitation@ '+
+        var options = {'title':'Annual February Precipitation @ '+
                                marker.getPosition().toString(),
-                       'width':400,
+                       'width':650,
                        'height':200,
 					   'legend': 'none',
 					   'vAxis': {title: "Precipitation (mm)"},
 					   'hAxis': {title: "Year"},
-					   'is3D': true,
-					   'displayAnnotations': true
-					   //'hAxis': {maxValue: 1987}
+					  
+					   'displayAnnotations': true,
+					   'pointSize': 5,
+					   'trendlines': { 0: {} },
+					   'hAxis.showTextEvery':2,
+					   'hAxis.gridlines':6
 					   };
                        
         var node        = document.createElement('div'),
@@ -94,32 +61,47 @@ dojo.require("dojo.parser");
 	  
 		//my style Json
 		var styles = [
-		{
-			"stylers": [
-				{ "invert_lightness": true },
-				{ "weight": 3 },
-				{ "hue": "#ff0900" },
-				{ "saturation": 28 },
-				{ "lightness": 25 },
-				{ "gamma": 0.79 }
-			]
-  
-		}
-		];
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#22bcd1" },
+      { "lightness": 57 }
+    ]
+  },{
+    "featureType": "landscape.natural.landcover",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#747d75" },
+      { "lightness": 76 }
+    ]
+  },{
+    "featureType": "landscape.natural.terrain",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#808080" },
+      { "lightness": 32 }
+    ]
+  },{
+  }
+];
 		//my styled map object
 		var styledMap = new google.maps.StyledMapType(styles,
 			{name: "My Style"});
 		//point object used for locating the map center
-		var myLatlng = new google.maps.LatLng(42.2510, -71.8232);
+		var myLatlng = new google.maps.LatLng(20, 0);
 		//costumed my map option
         var mapOptions = {
 		  //map center
           center: myLatlng,
 		  //map center
-          zoom: 4,
-		  //costumed map control options
+          zoom: 3,
+		  //custom map control options
 		  mapTypeControlOptions: {
-			//three styles including costumed one 
+			//three styles including custom one 
 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE,'map_style'],
 			//change it to drop down style
 			style:google.maps.MapTypeControlStyle.DROPDOWN_MENU 
@@ -166,12 +148,12 @@ dojo.require("dojo.parser");
 				
 			}
 			marker = new google.maps.Marker({
-			icon:"https://866943e6-a-62cb3a1a-s-sites.googlegroups.com/site/fanjian5i5i/home/jinfei.gif",
+			icon: 'https://866943e6-a-62cb3a1a-s-sites.googlegroups.com/site/fanjian5i5i/home/cloud3.gif',
 			animation:true,
             map: map,
             position: new google.maps.LatLng(lat, lng),
           });
-		  //console.log(lat,lng);
+	
           google.maps.event.addListener(marker, 'click', function() {
           drawChart(this,inArray);
           });
@@ -180,11 +162,18 @@ dojo.require("dojo.parser");
 		
 		map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+		google.maps.event.addListener(map, 'mousemove', function (event) {
+			mouseLat = event.latLng.lat();
+			mouseLng = event.latLng.lng();
+			var element = document.getElementById("footer");
+			element.innerHTML=mouseLat.toString().substring(0,5)+','+mouseLng.toString().substring(0,6);
+		});
 		google.maps.event.addListener(map,'click',function(event) {
 		mouseLat = event.latLng.lat();
 		mouseLng = event.latLng.lng();
-		//console.log(mouseLat,mouseLng);
-		
+		var element=document.getElementById("footer");
+		element.innerHTML=mouseLat+','+mouseLng;
+			
 		var table = "1vSi3EUO-AfDcnLpICxecb__B1nBLGh2JGYNKlpwB"
 		var sqlString = "SELECT * FROM " + table +" WHERE 'Lat' < " + (mouseLat + 1) + " AND 'Lat' > " + (mouseLat - 1) + " AND 'Long' > " + (mouseLng - 1) + " AND 'Long' < " + (mouseLng + 1);
 		var sql = encodeURIComponent(sqlString);
@@ -193,10 +182,9 @@ dojo.require("dojo.parser");
 		
 		query.send(function(response) {
           var dt = response.getDataTable();
-			//console.log(response);
+
           // For each row in the table, create a marker
 		  var categories = new Array();
-		  
           for (var i = 0; i < dt.getNumberOfRows(); i++) {
 			var infoArray = new Array();
 			var highData = new Array();
@@ -204,10 +192,12 @@ dojo.require("dojo.parser");
 			{
 				categories[j] = (j+1988).toString();
 				infoArray[j] = dt.getValue(i,j);
+			
 				if(j>=2)
 				{
 					highData[j-2] = dt.getValue(i,j); 
 				};
+			
 				var lat = dt.getValue(i,1);
 				var lng = dt.getValue(i,0);
 
@@ -216,9 +206,9 @@ dojo.require("dojo.parser");
 			
 			
 			$(function () {
-        $('#borderContainerTwo').highcharts({
+				$('#borderContainerTwo').highcharts({
             title: {
-                text: 'Annual Average Precipitation',
+                text: 'Annual February Precipitation',
                 x: -20 //center
             },
             subtitle: {
@@ -253,7 +243,7 @@ dojo.require("dojo.parser");
         });
 		});
 			
-		//fire up costumed styled map 
+		//fire up custom styled map 
 		map.mapTypes.set('map_style', styledMap);
 		map.setMapTypeId('map_style');	
 		
@@ -262,8 +252,7 @@ dojo.require("dojo.parser");
 
         map.setOptions({ draggableCursor: 'url(https://866943e6-a-62cb3a1a-s-sites.googlegroups.com/site/fanjian5i5i/home/yu.png), move' });
       });
-	fishSwim();
-	fishSwim1();
+
 	};
 	
 	
